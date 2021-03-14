6f8f41dcc1f05b99b2203caf094b3245e8953df5
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-03-14 03:59:02 +0200

    Finish UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.indigo,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 60.0,
@@ -24,7 +25,97 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 30,
                   color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: 2,
+                  fontFamily: 'Main',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SubMain',
+                  fontSize: 20,
+                  letterSpacing: 5,
+                  color: Colors.white70,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Divider(
+                color: Colors.white,
+                thickness: 1,
+                indent: 30,
+                height: 30,
+                endIndent: 30,
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+20 101 671 6496',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 14,
+                    backgroundImage: AssetImage(
+                        'assets/images/google-logo.png'
+                      ),
+                    backgroundColor: Colors.white,
+                    ),
+                  title: Text(
+                    'amr.abdalfatah.mohamed@gmail.com',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.indigo,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Main'
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: SizedBox(
+                    width: 30,
+                    height: 30,
+                    child: Image.asset(
+                        'assets/images/twitter.jpg',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  title: Text(
+                    '@Amr3bdalfatah',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.indigo,
+                        fontWeight: FontWeight.w600,
+                    ),
+                  ),
                 ),
               ),
             ],
